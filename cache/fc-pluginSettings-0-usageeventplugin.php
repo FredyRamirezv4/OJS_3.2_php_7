c0a086018fc810a95fdbb51ae2c85cb336ec0b04
Fredy Ramirez <jframirez209@misena.edu.co>
2023-04-18 19:22:22 -0500

    Instalacion inicial de ojs con una revista prueba

---
+++ cache/fc-pluginSettings-0-usageeventplugin.php
@@ -0,0 +1,4 @@
+<?php return array (
+  'enabled' => true,
+  'uniqueSiteId' => '643ed96327017',
+);
---
+++ cache/fc-pluginSettings-1-defaultthemeplugin.php
@@ -0,0 +1,7 @@
+<?php return array (
+  'baseColour' => '#ECD4E7',
+  'enabled' => true,
+  'showDescriptionInJournalIndex' => 'true',
+  'typography' => 'notoSerif',
+  'useHomepageImageAsHeader' => 'true',
+);
